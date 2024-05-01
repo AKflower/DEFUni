b89aa137a1dde1204017acb7b962d80e852e1578
Senthong <155158365+Senthong@users.noreply.github.com>
2024-05-01 14:38:20 +0700

    Delete app/front-end/src/app/mycourse directory

--- app/front-end/src/app/mycourse/mycourse.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './mycourse.module.scss'
-
-export default function mycourse() {
-    return (
-        <div className={styles.testCss}>My course</div>
-    )
-}
--- app/front-end/src/app/mycourse/page.tsx
+++
@@ -1,8 +0,0 @@
-import Mycourse from "./mycourse";
-
-export default function mycoursePage() {
-    return (
-        <mycourse />
-    )
-}
-
